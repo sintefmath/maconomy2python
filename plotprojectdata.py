44d022c2285e4c798d1e517c5a529a1b78063e2b
Franz Fuchs <franzgeorgfuchs@gmail.com>
2020-09-05 22:34:05 +0200

    update

--- plotprojectdata.py
+++ plotprojectdata.py
@@ -54,7 +54,10 @@ def getemployeesbillingprice(employees_by_number, fn):
                 billing_index=row.index('Billing Price')
                 date_index=row.index('Entry Date')
             else:
-                data[row[emplno_index]][row[date_index]] = int(row[billing_index].replace(" ", "").replace(",00",""))
+                if row[date_index] in data[row[emplno_index]]:
+                    data[row[emplno_index]][row[date_index]] += int(row[billing_index].replace(" ", "").replace(",00",""))
+                else:
+                    data[row[emplno_index]][row[date_index]] = int(row[billing_index].replace(" ", "").replace(",00",""))
             line_count += 1
     return data
 
@@ -83,25 +86,32 @@ if __name__ == "__main__":
     billings_by_employees_by_day = getemployeesbillingprice(employees_by_number, args.filename)
 
     billings_by_employees_total = {}
-    for employeenr, billings in billings_by_employees_by_day.items():
-        billings_by_employees_total[employeenr] = 0
-        for billingday, value in billings.items():
-            billings_by_employees_total[employeenr] += value
-
     billings_by_employees_by_month = {}
     for employeenr, billings in billings_by_employees_by_day.items():
+        billings_by_employees_total[employeenr] = 0
         billings_by_employees_by_month[employeenr] = np.zeros(12)
         for billingday, value in billings.items():
+            billings_by_employees_total[employeenr] += value
             date = datetime.datetime.strptime(billingday, "%d.%m.%Y")
             billings_by_employees_by_month[employeenr][date.month-1] += value
 
-    labels_month=('Jan','Feb','Mar','Apr','Mai','Jun','Jul','Aug','Sep','Oct','Nov','Des')
-
-#### actuals per month
     billings_by_month = np.zeros(12)
     for billingday, billings in billings_by_day.items():
         date = datetime.datetime.strptime(billingday, "%d.%m.%Y")
         billings_by_month[date.month-1] += billings
+
+    labels_month=('Jan','Feb','Mar','Apr','Mai','Jun','Jul','Aug','Sep','Oct','Nov','Des')
+
+    plt.rcParams["figure.figsize"] = 12,8
+    plt.rcParams["axes.titlesize"] = 24
+    plt.rcParams["axes.labelsize"] = 20
+    plt.rcParams["lines.linewidth"] = 3
+    plt.rcParams["lines.markersize"] = 10
+    plt.rcParams["xtick.labelsize"] = 16
+    plt.rcParams["ytick.labelsize"] = 16
+    plt.style.use('bmh')
+
+#### actuals per month
     fig1, ax1 = plt.subplots()
     ax1.bar(np.linspace(1,12,12),billings_by_month/1000)
     ax1.set_ylabel('KNOK')
@@ -112,6 +122,29 @@ if __name__ == "__main__":
         ax1.legend()
     ax1.set_title('Actuals per month')
     plt.savefig("actuals_per_month.png")
+
+    ### per employee
+    fig, (ax1,lax) = plt.subplots(ncols=2, gridspec_kw={"width_ratios":[4,1]})
+    bot = np.zeros(12)
+    for a, b in billings_by_employees_by_month.items():
+        ax1.bar(np.linspace(1,12,12),b/1000, bottom=bot/1000, label=employees_by_number[a])
+        bot+=b
+    ax1.set_ylabel('KNOK')
+    ax1.set_xticks(np.arange(1,13))
+    ax1.set_xticklabels(labels_month)
+    if args.totalbudget:
+        ax1.plot([1,12], [args.totalbudget/12, args.totalbudget/12],':k',label='mean')
+    h,l = ax1.get_legend_handles_labels()
+    lax.legend(h,l, borderaxespad=0)
+    lax.axis("off")
+    ax1.set_title('Actuals per month per employee')
+    plt.tight_layout()
+    plt.savefig("actuals_per_month_per_employee.png")
+
+    today = datetime.datetime.today()
+    week=today.isocalendar()[1]
+    month=today.month
+    year=today.year
 
     today = datetime.datetime.today()
     week=today.isocalendar()[1]
@@ -146,6 +179,47 @@ if __name__ == "__main__":
         ax1.legend()
     ax1.set_title('Actuals accumulated per month')
     plt.savefig("actuals_accumulated_per_month.png")
+
+    ### per employee
+    fig1, ax1 = plt.subplots()
+    bot = np.zeros(12)
+    for a, b in billings_by_employees_by_month.items():
+        bc = np.cumsum(b)
+        bc[month:] = 0
+        ax1.bar(np.linspace(1,12,12),bc/1000, bottom=bot/1000, label=employees_by_number[a])
+        bot+=bc
+    ax1.set_ylabel('KNOK')
+    ax1.set_xticks(np.arange(1,13))
+    ax1.set_xticklabels(labels_month)
+    if args.totalbudget:
+        ax1.plot([1,12], [args.totalbudget, args.totalbudget],'-k',label='total budget')
+
+    if args.regressionON and month>2:
+        ydata = cumsum_billings_by_month[:month]
+        xdata = np.arange(1,month+1)
+        popt, pcov = curve_fit(linear_func, xdata, ydata)
+        ax1.plot(np.arange(1,13),linear_func(np.arange(1,13), popt[0], popt[1])/1000,':k',label='linear regression')
+        ax1.plot(12,linear_func(12, popt[0], popt[1])/1000,'ko')
+        ax1.legend()
+    ax1.set_title('Actuals accumulated per month per employee')
+    plt.savefig("actuals_accumulated_per_month_per_employee.png")
+
+    fig, (ax1,lax) = plt.subplots(ncols=2, gridspec_kw={"width_ratios":[4,1]})
+    bot = np.zeros(12)
+    for a, b in billings_by_employees_by_month.items():
+        ax1.bar(np.linspace(1,12,12),b/1000, bottom=bot/1000, label=employees_by_number[a])
+        bot+=b
+    ax1.set_ylabel('KNOK')
+    ax1.set_xticks(np.arange(1,13))
+    ax1.set_xticklabels(labels_month)
+    if args.totalbudget:
+        ax1.plot([1,12], [args.totalbudget/12, args.totalbudget/12],':k',label='mean')
+    h,l = ax1.get_legend_handles_labels()
+    lax.legend(h,l, borderaxespad=0)
+    lax.axis("off")
+    ax1.set_title('Actuals per month per employee')
+    plt.tight_layout()
+    plt.savefig("actuals_per_month_per_employee.png")
 
 
 #### actuals per week
